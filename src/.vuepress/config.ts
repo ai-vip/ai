aa1a1e0ba8ac8775889f23317377299356847024
chaocloud <840558273@qq.com>
2023-04-01 17:46:13 +0800

    first

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,24 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Docs Demo",
+      description: "A docs demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "阿超的AI之旅",
+      description: "vuepress-theme-hope 的文档演示",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
